56925c5f16d0305503917c02c876f9ccb32c919e
Fabricio Catae <fcatae@hotmail.com>
2019-06-05 18:20:27 -0300

    add default get

--- SimpleBotCore/Controllers/MessagesController.cs
+++ SimpleBotCore/Controllers/MessagesController.cs
@@ -9,6 +9,12 @@ namespace SimpleBotCore.Controllers
     [Route("api/[controller]")]
     public class MessagesController : Controller
     {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+
         // POST api/messages
         [HttpPost]
         public void Post([FromBody]string value)
