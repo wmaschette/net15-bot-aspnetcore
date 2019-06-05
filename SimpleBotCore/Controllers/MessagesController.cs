c15437d8f44c62d8b214b7bf5482e681167e7435
Fabricio Catae <fcatae@hotmail.com>
2019-06-05 18:28:58 -0300

    add simple logic

--- SimpleBotCore/Controllers/MessagesController.cs
+++ SimpleBotCore/Controllers/MessagesController.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+using SimpleBotCore.Logic;
 
 namespace SimpleBotCore.Controllers
 {
     [Route("api/[controller]")]
     public class MessagesController : Controller
     {
+        static SimpleBotUser g_bot = new SimpleBotUser();
+
         [HttpGet]
         public string Get()
         {
@@ -17,8 +22,38 @@ namespace SimpleBotCore.Controllers
 
         // POST api/messages
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task<IActionResult> Post([FromBody]Activity activity)
         {
+            if (activity != null && activity.Type == ActivityTypes.Message)
+            {
+                await HandleActivityAsync(activity);
+            }
+
+            // HTTP 202
+            return Accepted();
+        }
+
+        // Estabelece comunicacao entre o usuario e o SimpleBotUser
+        async Task HandleActivityAsync(Activity activity)
+        {
+            string text = activity.Text;
+            string userFromId = activity.From.Id;
+            string userFromName = activity.From.Name;
+
+            var message = new SimpleMessage(userFromId, userFromName, text);
+
+            string response = g_bot.Reply(message);
+
+            await ReplyUserAsync(activity, response);
+        }
+
+        // Responde mensagens usando o Bot Framework Connector
+        async Task ReplyUserAsync(Activity message, string text)
+        {
+            var connector = new ConnectorClient(new Uri(message.ServiceUrl));
+            var reply = message.CreateReply(text);
+
+            await connector.Conversations.ReplyToActivityAsync(reply);
         }
     }
 }
