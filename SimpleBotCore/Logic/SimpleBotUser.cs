97427579e5774342b08dc07180b68edcce009461
Wallace Maschette <wmaschette@gmail.com>
2019-06-05 22:36:01 -0300

    ok

--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace SimpleBotCore.Logic
 {
@@ -9,7 +11,34 @@ namespace SimpleBotCore.Logic
     {
         public string Reply(SimpleMessage message)
         {
-            return $"{message.User} disse '{message.Text}'";
+            switch(message.Text.ToUpper())
+            {
+                case "OI":
+                    return $"Oi {message.User}, tudo bem?";
+                case "TUDO ÓTIMO E COM VOCÊ?":
+                    return $"Estou bem também, pode me informar o seu cpf?";
+                default:
+                    var dados = new BsonDocument()
+                    {
+                        { "Usuario", message.User },
+                        { "CPF", message.Text }
+                    };
+                    SalvarDados(dados);
+                    return $"Obrigado pela informação {message.User}!";
+
+            }
+        }
+
+        protected void SalvarDados(BsonDocument dados)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("15net");
+            var col = db.GetCollection<BsonDocument>("col01");
+
+            var doc = new BsonDocument();
+
+            col.InsertOne(dados);
         }
 
     }
